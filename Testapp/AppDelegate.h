f54f251f675c5e654229950a38f19349cd767def
中村　龍太郎 <ryu@nakamuraryuutarou-no-MacBook-Air.local>
2014-12-02 21:14:12 +0900

    Initial Commit

---
+++ Testapp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Testapp
+//
+//  Created by 中村　龍太郎 on 2014/12/02.
+//  Copyright (c) 2014年 中村　龍太郎. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Testapp/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Testapp
+//
+//  Created by 中村　龍太郎 on 2014/12/02.
+//  Copyright (c) 2014年 中村　龍太郎. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
